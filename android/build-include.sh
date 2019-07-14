b8b7961dfcb3f846a72f968aa0bad3799b8d7977
Martin <info@perry.cz>
2019-07-14 15:12:52 +0200

    bugfix

--- android/build-include.sh
+++ android/build-include.sh
@@ -1,35 +1,37 @@
 
+export API_VERSION=0
+
 ####################################################
 # Install standalone toolchain x86
 
-export API_VERSION=${API_VERSION_x86}
-build "x86" "x86" "x86" "i686-linux-android" ""
+API_VERSION=${API_VERSION_x86}
+build "x86" "x86" "x86" "i686-linux-android" "" "android-x86"
 
 
 ####################################################
 # Install standalone toolchain x86_64
 
-export API_VERSION=${API_VERSION_x64}
-build "x86_64" "x86_64" "x86_64" "x86_64-linux-android" ""
+API_VERSION=${API_VERSION_x64}
+build "x86_64" "x86_64" "x86_64" "x86_64-linux-android" "" "android64"
 
 
 ################################################################
 # Install standalone toolchain ARMeabi
 
-export API_VERSION=${API_VERSION_x86}
-build "ARMeabi" "armeabi" "arm" "arm-linux-androideabi" ""
+API_VERSION=${API_VERSION_x86}
+build "ARMeabi" "armeabi" "arm" "arm-linux-androideabi" "" "android"
 
 ################################################################
 # Install standalone toolchain ARMeabi-v7a
 
-export API_VERSION=${API_VERSION_x86}
-build "ARMeabi-v7a" "armeabi-v7a" "arm" "arm-linux-androideabi" "-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3"
+API_VERSION=${API_VERSION_x86}
+build "ARMeabi-v7a" "armeabi-v7a" "arm" "arm-linux-androideabi" "-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3" "android"
 
 ################################################################
 # Install standalone toolchain ARM64-v8a
 
-export API_VERSION=${API_VERSION_x64}
-build "ARM64-v8a" "arm64-v8a" "arm64" "aarch64-linux-android" ""
+API_VERSION=${API_VERSION_x64}
+build "ARM64-v8a" "arm64-v8a" "arm64" "aarch64-linux-android" "" "android64-aarch64"
 
 ################################################################
 # Install standalone toolchain MIPS
